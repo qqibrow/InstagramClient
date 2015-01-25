7b9b0903dad45bc7406618f171fd89980af27d39
qqibrow <qqibrow@gmail.com>
2015-01-24 23:15:27 -0800

    add round image.

--- app/src/main/java/exercise/lniu/instagramclient/InstagramPhoto.java
+++ app/src/main/java/exercise/lniu/instagramclient/InstagramPhoto.java
@@ -13,12 +13,16 @@ public class InstagramPhoto {
     public int imageHeight;
     public int likesCount;
 
+    public String profileImgUrl;
+
     static InstagramPhoto InstogramPhotoBuilder(JSONObject jsonObject) throws JSONException {
         InstagramPhoto photo = new InstagramPhoto();
         photo.username = jsonObject.getJSONObject("user").getString("username");
-        if(jsonObject.getJSONObject("caption") != null) {
+
+        if(jsonObject.getJSONObject("caption") != null && jsonObject.getJSONObject("caption").getString("text") != null) {
             photo.caption = jsonObject.getJSONObject("caption").getString("text");
         }
+        photo.profileImgUrl = jsonObject.getJSONObject("user").getString("profile_picture");
         photo.imageUrl = jsonObject.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
         photo.imageHeight = jsonObject.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
         photo.likesCount = jsonObject.getJSONObject("likes").getInt("count");
--- app/src/main/java/exercise/lniu/instagramclient/InstagramPhotosAdapter.java
+++ app/src/main/java/exercise/lniu/instagramclient/InstagramPhotosAdapter.java
@@ -1,6 +1,7 @@
 package exercise.lniu.instagramclient;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,7 +9,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.makeramen.RoundedImageView;
+import com.makeramen.RoundedTransformationBuilder;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import java.util.ArrayList;
 
@@ -28,15 +32,30 @@ public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
         if(convertView == null)
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
 
+        // Get all views.
         TextView tvCaption = (TextView)convertView.findViewById(R.id.tvCaption);
         ImageView imgPhoto = (ImageView)convertView.findViewById(R.id.imgPhoto);
+        RoundedImageView roundImgUser = (RoundedImageView)convertView.findViewById(R.id.roundImgUser);
         if(photo.caption != null)
             tvCaption.setText(photo.caption);
         imgPhoto.getLayoutParams().height = photo.imageHeight;
         // reset image from the recycled view.
         imgPhoto.setImageResource(0);
         // Background: Ask for the photo to be added in the imageView on the photo url.
+        Picasso.with(getContext()).load(photo.profileImgUrl).into(roundImgUser);
         Picasso.with(getContext()).load(photo.imageUrl).into(imgPhoto);
+//        Transformation transformation = new RoundedTransformationBuilder()
+//                .borderColor(Color.BLACK)
+//                .borderWidthDp(3)
+//                .cornerRadiusDp(30)
+//                .oval(false)
+//                .build();
+
+//        Picasso.with(getContext())
+//                .load("https://igcdn-photos-b-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-19/10881954_647074052068465_483507547_a.jpg")
+//                .fit()
+//                .transform(transformation)
+//                .into(roundImgUser);
         return convertView;
     }
 }
